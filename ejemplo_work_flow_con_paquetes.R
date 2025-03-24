0e2b7ff0e3a36f0438866033121a0f9dab19f190
Ludwing-MJ <145233672+Ludwing-MJ@users.noreply.github.com>
2025-03-24 15:01:31 -0600

    version final

--- ejemplo_work_flow_con_paquetes.R
+++ ejemplo_work_flow_con_paquetes.R
@@ -17,20 +17,21 @@ if (!require("rstudioapi")) install.packages("rstudioapi")
 altura_pino <- read_excel("datos_arboles.xlsx")
 
 # Análisis de varianza
-modelo_anova <- aov(altura_ft ~ tratamiento, data = datos_rendimiento)
-modelo_anova
+modelo_anova <- aov(altura_ft ~ tratamiento, data = altura_pino)
+summary(modelo_anova)
 
-# Prueba de comparación múltiple (Tukey)
+# Prueba de Tukey
 comparacion_tukey <- HSD.test(modelo_anova, "tratamiento")
 print(comparacion_tukey)
 
 # Visualización de resultados
 ggplot(altura_pino, aes(x = tratamiento, y = altura_ft, fill = tratamiento)) +
   geom_boxplot() +
-  theme_bw() +
   labs(title = "Altura por Tratamiento",
        x = "Tratamiento",
-       y = "Altura en pies")
+       y = "Altura en pies")+
+  theme_minimal()+ # Establece el tema del gráfico 
+  theme(legend.position = "none")  # Remueve la leyenda redundante
 
 # Exportar resultados a Excel
 write_xlsx(comparacion_tukey$groups, "resultados_tukey.xlsx", col_names = T, format_headers = T, use_zip64 = F)
